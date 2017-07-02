68d37593f16fa625ec9e312a4ee182e7fc6d92d3
fkanyako <fkanyako@gmail.com>
2017-07-02 22:46:29 +0200

    Inheritance

---
+++ Inheritance.py
@@ -0,0 +1,44 @@
+#Methods
+class fighter(object):
+	"""docstring for fighter"""
+	def __init__(self, name):
+		self.name = name
+		self.health = 100
+		self.damage  = 10
+
+
+	def attack(self, other):
+		other.health -= self.damage
+		print("{}: attacks {}!".format(self.name, other.name))
+		print("{}: losses {} healht point!".format(other.name, self.damage))
+
+
+
+
+	def __str__(self):    #String represenation
+		return "{}: {}".format(self.name, self.health)
+
+
+
+
+Frank = fighter("Frank")
+James = fighter("James")
+
+print(Frank)
+print(James)
+
+James.attack(Frank)
+print(Frank)
+
+class Boxer(fighter):
+	pass
+
+
+boxer_frank = Boxer("Boxer_Frank")
+print(boxer_frank)
+print("Health points:", boxer_frank.health)
+print("Damage points:",boxer_frank.damage)
+
+
+
+
