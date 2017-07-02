503a6a36a9e1d04616bb2faaf240f731c70f0e50
fkanyako <fkanyako@gmail.com>
2017-07-02 14:07:18 +0200

    First lesson on OOP python

---
+++ clev.pr_INIT.py
@@ -0,0 +1,23 @@
+class Student(object):
+	"""docstring for Student"""
+	def __init__(self, name, grade, age):
+		self.name = name
+		self.grade = grade
+		self.age = age
+
+
+James = Student("James", 85, 26)
+Fati = Student("Fatimata", 70, 19)
+
+
+print(James.name)
+print(Fati.name)
+		
+
+print(James.grade)
+print(Fati.grade)
+
+print(James.age)
+print(Fati.age)
+
+		
