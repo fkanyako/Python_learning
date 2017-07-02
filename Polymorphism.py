f967ec7128565ebbefaca93d49cc8737244f0725
fkanyako <fkanyako@gmail.com>
2017-07-02 23:59:57 +0200

    Referencing other classes

---
+++ Polymorphism.py
@@ -0,0 +1,32 @@
+# Polymorphism
+class Animal(object):
+	"""docstring for Animal"""
+	def __init__(self, name):
+		self.name = name
+
+
+	def talk(self):
+		pass
+
+
+class Dog(Animal):
+	def talk(self):
+		return "WOOF WOOF"
+
+
+class Cat(Animal):
+	def talk(self):
+		return "MEOW MEOW"
+
+
+
+pet1 = Dog("Remy")
+print(pet1.name)
+print(pet1.talk())
+
+print("")
+pet2 = Cat("Cynthia")
+print(pet2.name)
+print(pet2.talk())
+		
+
