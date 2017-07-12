8c8f2fc64f95fe71350c433b59811039463a6ec8
fkanyako <fkanyako@gmail.com>
2017-07-13 01:31:37 +0200

    Age App

--- person_class.py
+++ person_class.py
@@ -8,7 +8,13 @@ class Person:
         self.name = name
         self.birthdate = birthdate
 
+    def age(self):
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+        return age
+
 Lansana = Person("Hassan", datetime.date(1992, 7, 5))
 print(Lansana.name)
 print(Lansana.birthdate)
+print(Lansana.age())
 
